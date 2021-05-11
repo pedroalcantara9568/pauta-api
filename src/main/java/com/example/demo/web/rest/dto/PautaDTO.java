258aecb77605f982acbc4a62d365bd6249953577
cabeca <pedro.alcantra@castgroup.com.br>
2021-05-11 00:50:07 -0300

    alteração da documentação swagger

--- src/main/java/com/example/demo/web/rest/dto/PautaDTO.java
+++ src/main/java/com/example/demo/web/rest/dto/PautaDTO.java
@@ -1,6 +1,5 @@
 package com.example.demo.web.rest.dto;
 
-import com.example.demo.domain.voto.Voto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
@@ -9,7 +8,6 @@ import lombok.Setter;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.Set;
 
 @Getter
 @Setter
@@ -19,7 +17,6 @@ public class PautaDTO implements Serializable {
 
     private Long id;
     private String titulo;
-    Set<Voto> votos;
     private String status;
 
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss", shape = JsonFormat.Shape.STRING)
